107422fcfb351d2b3ee556680f6f05ff3bb5c66d
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2018-07-13 22:36:25 +0200

    upgrade all versions (node, libraries) and apply preferred formatting (4 spaces and no semicolons)

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var PropTypes = require('prop-types');
-var mobxReact = require('mobx-react');
-var I18n = require('i18n-harmony');
+var PropTypes = require('prop-types')
+var mobxReact = require('mobx-react')
+var I18n = require('i18n-harmony')
 
 function defaultInjectFn(stores) {
-  return {
-    i18n: stores.i18n
-  };
+    return {
+        i18n: stores.i18n
+    }
 }
 
 var config = {
-  injectFn: defaultInjectFn
-};
+    injectFn: defaultInjectFn
+}
 
 function init(injectFn) {
-  config.injectFn = injectFn || defaultInjectFn;
+    config.injectFn = injectFn || defaultInjectFn
 }
 
 /**
@@ -25,54 +25,54 @@ function init(injectFn) {
  */
 function translatable(Component, connect) {
 
-  /* istanbul ignore if */
-  if (Component.propTypes) {
-    Component.propTypes.i18n = PropTypes.object;
-  } else {
-    Component.propTypes = {
-      i18n: PropTypes.object
-    };
-  }
+    /* istanbul ignore if */
+    if (Component.propTypes) {
+        Component.propTypes.i18n = PropTypes.object
+    } else {
+        Component.propTypes = {
+            i18n: PropTypes.object
+        }
+    }
 
-  Component.prototype.t = function(key, opts) {
-    var i18n = this.props.i18n;
+    Component.prototype.t = function (key, opts) {
+        var i18n = this.props.i18n
 
-    // Third argument is the locale that should be used
-    // We can't use the active one because of the SSR and shared I18n state
-    // Also, we need to observe i18n.locale to detect changes, so this is useful
-    return I18n.t(key, opts, i18n.locale);
-  };
+        // Third argument is the locale that should be used
+        // We can't use the active one because of the SSR and shared I18n state
+        // Also, we need to observe i18n.locale to detect changes, so this is useful
+        return I18n.t(key, opts, i18n.locale)
+    }
 
-  Component.prototype.has = function(key, opts, includeDefault) {
-    var i18n = this.props.i18n;
+    Component.prototype.has = function (key, opts, includeDefault) {
+        var i18n = this.props.i18n
 
-    // Forth argument is the locale that should be used
-    // We can't use the active one because of the SSR and shared I18n state
-    // Also, we need to observe i18n.locale to detect changes, so this is useful
-    return I18n.has(key, opts, !!includeDefault, i18n.locale);
-  };
+        // Forth argument is the locale that should be used
+        // We can't use the active one because of the SSR and shared I18n state
+        // Also, we need to observe i18n.locale to detect changes, so this is useful
+        return I18n.has(key, opts, !!includeDefault, i18n.locale)
+    }
 
-  var observed = connect
-    ? mobxReact.observer(connect)(Component)
-    : mobxReact.observer(Component);
-  return mobxReact.inject(config.injectFn)(observed);
+    var observed = connect
+        ? mobxReact.observer(connect)(Component)
+        : mobxReact.observer(Component)
+    return mobxReact.inject(config.injectFn)(observed)
 }
 
 function translatorFactory(i18nStore) {
-  return function translate(key, opts) {
-      return I18n.t(key, opts, i18nStore.locale)
-  }
+    return function translate(key, opts) {
+        return I18n.t(key, opts, i18nStore.locale)
+    }
 }
 
 module.exports = {
-  init: init,
-  translatable: function(arg) {
-    if (arg instanceof Array) {
-      return function(Component) {
-        return translatable(Component, arg);
-      }
-    }
-    return translatable(arg);
-  },
-  tFactory: translatorFactory
+    init: init,
+    translatable: function (arg) {
+        if (arg instanceof Array) {
+            return function (Component) {
+                return translatable(Component, arg)
+            }
+        }
+        return translatable(arg)
+    },
+    tFactory: translatorFactory
 }
--- test.js
+++ test.js
@@ -1,149 +1,149 @@
-const {expect} = require('chai');
-const {describe, it} = require('mocha');
-const {mount} = require('enzyme');
+const { expect } = require('chai')
+const { describe, it } = require('mocha')
+const { mount } = require('enzyme')
 
-const React = require('react');
-const {Provider} = require('mobx-react');
-const {observable} = require('mobx');
-const i18n = require('i18n-harmony');
-const {jsdom} = require('jsdom');
+const React = require('react')
+const { Provider } = require('mobx-react')
+const { observable } = require('mobx')
+const i18n = require('i18n-harmony')
+const { jsdom } = require('jsdom')
 
-const {init, translatable, tFactory} = require('./index');
+const { init, translatable, tFactory } = require('./index')
 
-const exposedProperties = ['window', 'navigator', 'document'];
+const exposedProperties = ['window', 'navigator', 'document']
 
 function initMockDOM() {
-  global.document = jsdom('');
-  global.window = document.defaultView;
-  Object.keys(document.defaultView).forEach((property) => {
-    if (typeof global[property] === 'undefined') {
-      exposedProperties.push(property);
-      global[property] = document.defaultView[property];
-    }
-  });
+    global.document = jsdom('')
+    global.window = document.defaultView
+    Object.keys(document.defaultView).forEach((property) => {
+        if (typeof global[property] === 'undefined') {
+            exposedProperties.push(property)
+            global[property] = document.defaultView[property]
+        }
+    })
 
-  global.navigator = {
-    userAgent: 'node.js'
-  };
+    global.navigator = {
+        userAgent: 'node.js'
+    }
 }
 
-describe('react-mobx-translatable', function() {
-  beforeEach(function() {
-    i18n.init({
-      translations: {
-        en: {hello: 'Hello'},
-        de: {hello: 'Hallo'}
-      }
-    });
-    initMockDOM();
-  });
+describe('react-mobx-translatable', function () {
+    beforeEach(function () {
+        i18n.init({
+            translations: {
+                en: { hello: 'Hello' },
+                de: { hello: 'Hallo' }
+            }
+        })
+        initMockDOM()
+    })
 
-  it('should work with default configuration', function() {
-    const store = {
-      i18n: observable({locale: 'en'})
-    };
-    init();
+    it('should work with default configuration', function () {
+        const store = {
+            i18n: observable({ locale: 'en' })
+        }
+        init()
 
-    class MyComponent extends React.Component {
-      render() {
-        return <div>{this.t('hello')}</div>;
-      }
-    }
-    const MyWrappedComponent = translatable(MyComponent);
+        class MyComponent extends React.Component {
+            render() {
+                return <div>{this.t('hello')}</div>
+            }
+        }
+        const MyWrappedComponent = translatable(MyComponent)
 
-    const wrapper = mount(<Provider {...store}><MyWrappedComponent /></Provider>);
+        const wrapper = mount(<Provider {...store}><MyWrappedComponent /></Provider>)
 
-    expect(wrapper.find('div').first().text()).to.equal('Hello');
+        expect(wrapper.find('div').first().text()).to.equal('Hello')
 
-    store.i18n.locale = 'de';
-    expect(wrapper.find('div').first().text()).to.equal('Hallo');
-  });
+        store.i18n.locale = 'de'
+        expect(wrapper.find('div').first().text()).to.equal('Hallo')
+    })
 
-  it('should work with custom store configuration', function() {
-    const store = {
-      ui: {
-        i18n: observable({locale: 'en'})
-      }
-    };
-    init((store) => ({i18n: store.ui.i18n}));
+    it('should work with custom store configuration', function () {
+        const store = {
+            ui: {
+                i18n: observable({ locale: 'en' })
+            }
+        }
+        init((store) => ({ i18n: store.ui.i18n }))
 
-    class MyComponent extends React.Component {
-      render() {
-        return <div>{this.t('hello')}</div>;
-      }
-    }
-    const MyWrappedComponent = translatable(MyComponent);
+        class MyComponent extends React.Component {
+            render() {
+                return <div>{this.t('hello')}</div>
+            }
+        }
+        const MyWrappedComponent = translatable(MyComponent)
 
-    const wrapper = mount(<Provider {...store}><MyWrappedComponent /></Provider>);
+        const wrapper = mount(<Provider {...store}><MyWrappedComponent /></Provider>)
 
-    expect(wrapper.find('div').first().text()).to.equal('Hello');
+        expect(wrapper.find('div').first().text()).to.equal('Hello')
 
-    store.ui.i18n.locale = 'de';
-    expect(wrapper.find('div').first().text()).to.equal('Hallo');
-  });
+        store.ui.i18n.locale = 'de'
+        expect(wrapper.find('div').first().text()).to.equal('Hallo')
+    })
 
-  it('should work with custom connectors', function() {
-    const store = {
-      data: observable({
-        foo: 1
-      }),
-      ui: {
-        i18n: observable({locale: 'en'})
-      }
-    };
-    init((store) => ({i18n: store.ui.i18n}));
+    it('should work with custom connectors', function () {
+        const store = {
+            data: observable({
+                foo: 1
+            }),
+            ui: {
+                i18n: observable({ locale: 'en' })
+            }
+        }
+        init((store) => ({ i18n: store.ui.i18n }))
 
-    class MyComponent extends React.Component {
-      render() {
-        expect(this.props.data).to.be.an('object');
+        class MyComponent extends React.Component {
+            render() {
+                expect(this.props.data).to.be.an('object')
 
-        return <div>{this.t('hello')}</div>;
-      }
-    }
-    MyComponent.propTypes = {
-      data: React.PropTypes.object
-    };
-    const MyWrappedComponent = translatable(['data'])(MyComponent);
+                return <div>{this.t('hello')}</div>
+            }
+        }
+        MyComponent.propTypes = {
+            data: React.PropTypes.object
+        }
+        const MyWrappedComponent = translatable(['data'])(MyComponent)
 
-    const wrapper = mount(<Provider {...store}><MyWrappedComponent /></Provider>);
+        const wrapper = mount(<Provider {...store}><MyWrappedComponent /></Provider>)
 
-    expect(wrapper.find('div').first().text()).to.equal('Hello');
+        expect(wrapper.find('div').first().text()).to.equal('Hello')
 
-    store.ui.i18n.locale = 'de';
-    expect(wrapper.find('div').first().text()).to.equal('Hallo');
-  });
+        store.ui.i18n.locale = 'de'
+        expect(wrapper.find('div').first().text()).to.equal('Hallo')
+    })
 
-  it('has should return if translation exists', function() {
-    let helloExists = false;
-    const store = {
-      i18n: observable({locale: 'en'})
-    };
-    init();
+    it('has should return if translation exists', function () {
+        let helloExists = false
+        const store = {
+            i18n: observable({ locale: 'en' })
+        }
+        init()
 
-    class MyComponent extends React.Component {
-      componentWillMount() {
-        const existsKey = this.has('hello');
-        const doesntExistsKey = this.has('unknown-key');
+        class MyComponent extends React.Component {
+            componentWillMount() {
+                const existsKey = this.has('hello')
+                const doesntExistsKey = this.has('unknown-key')
 
-        expect(existsKey).to.be.eq(true);
-        expect(doesntExistsKey).to.be.eq(false);
-      }
+                expect(existsKey).to.be.eq(true)
+                expect(doesntExistsKey).to.be.eq(false)
+            }
 
-      render() {
-        return <div>{this.t('hello')}</div>;
-      }
-    }
-    const MyWrappedComponent = translatable(MyComponent);
+            render() {
+                return <div>{this.t('hello')}</div>
+            }
+        }
+        const MyWrappedComponent = translatable(MyComponent)
 
-    const wrapper = mount(<Provider {...store}><MyWrappedComponent /></Provider>);
-  });
+        const wrapper = mount(<Provider {...store}><MyWrappedComponent /></Provider>)
+    })
 
-  it('should provide factory for translation function', function() {
-    const store = {
-      i18n: observable({locale: 'en'})
-    };
-    init();
+    it('should provide factory for translation function', function () {
+        const store = {
+            i18n: observable({ locale: 'en' })
+        }
+        init()
 
-    expect(tFactory(store.i18n)('hello')).to.equal('Hello')
-  });
-});
+        expect(tFactory(store.i18n)('hello')).to.equal('Hello')
+    })
+})
